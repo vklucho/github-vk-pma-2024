14ef97b64a4b6c5d083c0d99364d00eb6b472465
Vojtěch Klucho <vojtech.klucho@tul.cz>
2024-09-26 15:24:56 +0200

    zmeny

--- MyApp001LinearLayout/app/src/main/java/com/example/myapp001linearlayout/MainActivity.kt
+++ MyApp001LinearLayout/app/src/main/java/com/example/myapp001linearlayout/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapp001linearlayout
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,40 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        } */
+
+        val etName = findViewById<EditText>(R.id.etName)
+        val etSurname = findViewById<EditText>(R.id.etSurname)
+        val etPlace = findViewById<EditText>(R.id.etPlace)
+        val etAge = findViewById<EditText>(R.id.etAge)
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        val btnDelete = findViewById<Button>(R.id.btnDelete)
+        val tvInformation = findViewById<TextView>(R.id.tvInformation)
+
+        btnSend.setOnClickListener{
+            val name = etName.text.toString()
+            val surname = etSurname.text.toString()
+            val place = etPlace.text.toString()
+            val age = etAge.text.toString()
+
+            val formattedText = "Jmenuji se $name $surname. Moje bydliste je $place. Je mi $age."
+            tvInformation.text = formattedText
+
+        }
+        btnDelete.setOnClickListener{
+            etName.text.clear()
+            etSurname.text.clear()
+            etPlace.text.clear()
+            etAge.text.clear()
+
+            tvInformation.text = ""
         }
     }
 }
