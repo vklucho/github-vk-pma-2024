9fbe33b415e1a23b27a0f26e351b640b81b113fe
Vojtěch Klucho <vojtech.klucho@tul.cz>
2024-10-17 11:28:41 +0200

    #6 fungující snack i toast

--- MyApp007ToastSnackbar/app/src/main/java/com/example/myapp007toastsnackbar/MainActivity.kt
+++ MyApp007ToastSnackbar/app/src/main/java/com/example/myapp007toastsnackbar/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.myapp007toastsnackbar
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp007toastsnackbar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnnShowToast.setOnClickListener{
+            val toast = Toast.makeText(this, "nazdar - mám hlad", Toast.LENGTH_LONG)
+
+            toast.show()
+        }
+
+        binding.btnShowSnackbar.setOnClickListener{
+            val snackbar = Snackbar.make(it, "toto je snackbar", Snackbar.LENGTH_LONG)
+
+            snackbar.show()
         }
     }
 }
