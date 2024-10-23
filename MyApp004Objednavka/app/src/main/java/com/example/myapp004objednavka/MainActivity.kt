0e147fe0cc1d7ce7dffa9a21c0ab24cbf0c4990d
Vojtěch Klucho <vojtech.klucho@tul.cz>
2024-10-23 16:33:49 +0200

    #6 Implementace toastíku a snackbaru v komplikovanějším projektu

--- MyApp004Objednavka/app/src/main/java/com/example/myapp004objednavka/MainActivity.kt
+++ MyApp004Objednavka/app/src/main/java/com/example/myapp004objednavka/MainActivity.kt
@@ -7,9 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.Toast
 import com.example.myapp004objednavka.databinding.ActivityMainBinding
-
-
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,39 +22,64 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         title = "Objednávka vozidla"
 
-        binding.btnSend.setOnClickListener{
-            //načtení ID vybraného radiobuttonu z radiogroup
-            val carRbId = binding.rgCar.checkedRadioButtonId
-            val car = findViewById<RadioButton>(carRbId)
+        // Zobrazení Toastu při změně vybraného RadioButton
+        binding.rgCar.setOnCheckedChangeListener { _, checkedId ->
+            val selectedRadioButton = findViewById<RadioButton>(checkedId)
+            val selectedText = selectedRadioButton.text.toString()
 
-            val prevodovka = binding.cbPrevodovka.isChecked
-            val volant = binding.cbVolant.isChecked
-            val strecha = binding.cbStrecha.isChecked
+            // Vytvoření Toastu
+            Toast.makeText(this, "Vybrali jste: $selectedText", Toast.LENGTH_SHORT).show()
 
-            val objednavkaText = "Souhrn objednávky: " +
-                    "$car.text" +
-                    (if(prevodovka)"; prevodovka" else "") +
-                    (if(volant)"; volant" else "") +
-                    (if(strecha)"; strecha" else "")
+            binding.btnSend.setOnClickListener {
+                //načtení ID vybraného radiobuttonu z radiogroup
+                val carRbId = binding.rgCar.checkedRadioButtonId
+                val car = findViewById<RadioButton>(carRbId)
 
-            binding.tvSummary.text = objednavkaText
+                val prevodovka = binding.cbPrevodovka.isChecked
+                val volant = binding.cbVolant.isChecked
+                val strecha = binding.cbStrecha.isChecked
+
+                val objednavkaText = "Souhrn objednávky: " +
+                        "$car.text" +
+                        (if (prevodovka) "; prevodovka" else "") +
+                        (if (volant) "; volant" else "") +
+                        (if (strecha) "; strecha" else "")
+
+                binding.tvSummary.text = objednavkaText
+
+            }
+
+            //změna obrázku v závislosti na vybraném radiobuttonu
+
+            binding.rbBmw.setOnClickListener {
+                binding.ivCar.setImageResource(R.drawable.bmw)
+            }
+
+            binding.rbMercedes.setOnClickListener {
+                binding.ivCar.setImageResource(R.drawable.mercedes)
+            }
+
+            binding.rbVolkswagen.setOnClickListener {
+                binding.ivCar.setImageResource(R.drawable.volkswagen)
+            }
+
+            //Zobrazení snackbaru
+            binding.btnSend.setOnClickListener {
+                // Přidáme logiku pro zobrazení Snackbaru
+                val snackbar = Snackbar.make(it, "Objednávka byla odeslána!", Snackbar.LENGTH_LONG)
+
+                // Přidáme akci pro zavření Snackbaru
+                snackbar.setAction("Zavřít") {
+                    Toast.makeText(this, "Snackbar zavřen", Toast.LENGTH_SHORT).show()
+                }
+
+                // Změna barvy pozadí
+                snackbar.setBackgroundTint(resources.getColor(R.color.black))
+
+                // Zobrazíme Snackbar
+                snackbar.show()
+            }
 
         }
-
-        //změna obrázku v závislosti na vybraném radiobuttonu
-
-        binding.rbBmw.setOnClickListener{
-            binding.ivCar.setImageResource(R.drawable.bmw)
-        }
-
-        binding.rbMercedes.setOnClickListener{
-            binding.ivCar.setImageResource(R.drawable.mercedes)
-        }
-
-        binding.rbVolkswagen.setOnClickListener{
-            binding.ivCar.setImageResource(R.drawable.volkswagen)
-        }
-
-
     }
 }
