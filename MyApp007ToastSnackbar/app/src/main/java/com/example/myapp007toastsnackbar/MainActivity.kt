d5aeb0036e1bd898c80320997ae2338efeb93a8d
Vojtěch Klucho <vojtech.klucho@tul.cz>
2024-10-23 16:27:10 +0200

    #6 Hotový toast a snack

--- MyApp007ToastSnackbar/app/src/main/java/com/example/myapp007toastsnackbar/MainActivity.kt
+++ MyApp007ToastSnackbar/app/src/main/java/com/example/myapp007toastsnackbar/MainActivity.kt
@@ -2,36 +2,59 @@ package com.example.myapp007toastsnackbar
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.myapp007toastsnackbar.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnnShowToast.setOnClickListener{
-            val toast = Toast.makeText(this, "nazdar - mám hlad", Toast.LENGTH_LONG)
-
-            toast.show()
+        binding.btnShowToast.setOnClickListener {
+            showCustomToast("Nazdar - mám hlad")
         }
+    }
 
-        binding.btnShowSnackbar.setOnClickListener{
-            Snackbar.make(binding.root, "já jsem snackbar", Snackbar.LENGTH_LONG)
-                .setDuration(7000)
-                .setBackgroundTint(Color.parseColor("#ff3578"))
-                .setAction("zavřít"){
-                    Toast.makeText(this, "zavírám", Toast.LENGTH_SHORT).show()
-                }
-                .show()
+
+    private fun showCustomToast(message: String) {
+
+        val inflater = LayoutInflater.from(this)
+        val layout = inflater.inflate(R.layout.custom_toast, null)
+
+
+        val toastMessage: TextView = layout.findViewById(R.id.toast_message)
+        val toastIcon: ImageView = layout.findViewById(R.id.toast_icon)
+        toastMessage.text = message
+        toastIcon.setImageResource(R.drawable.toast)
+
+        val toast = Toast(this)
+        toast.duration = Toast.LENGTH_LONG
+        toast.view = layout
+        toast.show()
+
+        binding.btnShowSnackbar.setOnClickListener {
+
+            val snackbar = Snackbar.make(it, "Toto je Snackbar!", Snackbar.LENGTH_LONG)
+
+
+            snackbar.setDuration(7000)
+            snackbar.setBackgroundTint((Color.parseColor("#222222")))
+
+            snackbar.setAction("Zavřít") {
+                Toast.makeText(this, "Zavírám Snackbar", Toast.LENGTH_LONG).show()
+            }
+
+            snackbar.show()
+
         }
     }
 }
