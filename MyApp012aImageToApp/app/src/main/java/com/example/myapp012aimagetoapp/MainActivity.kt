f8eb7a1d52f58c7fd506107a8ec8ef974f035c7c
Vojtěch Klucho <vojtech.klucho@tul.cz>
2024-11-07 11:25:28 +0100

    #10 hotovo

--- MyApp012aImageToApp/app/src/main/java/com/example/myapp012aimagetoapp/MainActivity.kt
+++ MyApp012aImageToApp/app/src/main/java/com/example/myapp012aimagetoapp/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.myapp012aimagetoapp
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp012aimagetoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val getcontent = registerForActivityResult(ActivityResultContracts.GetContent()){
+            uri: Uri? -> binding.ivImage.setImageURI(uri)
         }
+
+        binding.btnTakeImage.setOnClickListener(){
+            getcontent.launch("image/*")
+        }
+
     }
 }
