b32fa4fcaf8901a7a343945030589938f0409219
vklucho <vojtech.klucho@tul.cz>
2025-01-04 23:48:21 +0100

    #12 HOTOVO

--- MyApp014aMyNoteHub/app/src/main/java/com/example/myapp014amynotehub/MainActivity.kt
+++ MyApp014aMyNoteHub/app/src/main/java/com/example/myapp014amynotehub/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-       super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
